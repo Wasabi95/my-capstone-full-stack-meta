2cf05c7a312abb3b1526348f4b00cd0aed0cfcba
Andrey Carvajal <ancarsa.wasabi.12@gmail.com>
2023-07-26 15:43:26 -0500

    amplify auth

--- src/components/pages/Bookings/index.js
+++ src/components/pages/Bookings/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import { fetchAPI, submitAPI } from '../../../utils/fakeAPI';
 import pages from '../../../utils/pages';
 import BookingForm from './BookingForm';
-// Import AWS Amplify components and methods
-import { API, Storage } from 'aws-amplify';
-import createReservation from "../../../graphql/mutations"
-
 
 
 const updateTimes = (availableTimes, date) => {
@@ -25,35 +21,10 @@ const Bookings = () => {
   ] = useReducer(updateTimes, [], initializeTimes);
   const navigate = useNavigate();
 
-// Update the submitData function to create the reservation and store it in S3
-const submitData = async formData => {
-  try {
-    // Create a new reservation object to be sent to the API
-    const newReservation = {
-      name: formData.name,
-      lastName: formData.lastName,
-      cellphone: formData.cellphone,
-      date: formData.date,
-      time: formData.time,
-      numberOfGuests: formData.numberOfGuests,
-      occasion: formData.occasion,
-    };
-
-    // Use the GraphQL mutation to create the reservation in DynamoDB
-    await API.graphql({ query: createReservation, variables: { input: newReservation } });
-
-    // Reservation created successfully, now upload the reservation data to S3
-    await Storage.put(`${formData.name}_${formData.lastName}_reservation.json`, JSON.stringify(newReservation));
-
-    // Navigate to the confirmed booking page
-    navigate(pages.get('confirmedBooking').path);
-
-  } catch (error) {
-    // Handle error (e.g., show error message)
-    console.error('Error creating reservation:', error);
-  }
-};
-
+  const submitData = formData => {
+    const response = submitAPI(formData);
+    if (response) navigate(pages.get('confirmedBooking').path);
+  }; 
 
   return (
     <div className="bookings">
